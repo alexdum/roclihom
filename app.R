3d8d9ca533f5a69433b2968b835774d1371b1ac9
alexdum <alexandru.dumitrescu@gmail.com>
2024-09-30 09:47:32 -0500

    verificare

--- app.R
+++ app.R
@@ -4,8 +4,6 @@ library(dplyr)
 library(ggplot2)
 
 
-
-
 df <- readr::read_csv("penguins.csv")
 # Find subset of columns that are suitable for scatter plot
 df_num <- df |> select(where(is.numeric), -Year)
