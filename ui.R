74845bfc9fd0c6cad8beedd292839988bf0419c9
alexdum <alexandru.dumitrescu@gmail.com>
2024-10-04 16:20:59 -0500

    add info tab

--- ui.R
+++ ui.R
@@ -1,12 +1,7 @@
-# Define the UI using bslib for a modern layout
 page_sidebar(
-  
   theme = bs_theme(version = 5, bootswatch = "cosmo"),
   
   sidebar = sidebar(
-    
-
-    
     # Dropdown to select station by name
     selectInput(
       inputId = "stationSelect",
@@ -22,6 +17,7 @@ page_sidebar(
       value = c(0, 2504),
       step = 1
     ),
+    
     selectInput(
       inputId = "variable",
       label = "Variable:",
@@ -70,11 +66,40 @@ page_sidebar(
       )
     )
   ),
-  card(
-    card_header(
-      h5(textOutput("map_title"))  # map title
-    ), 
-    leafletOutput("map", height = 300),
-    plotlyOutput("time_series_plot", height = "150px") # Add time series plot below the map
+  
+  # Adding the tabs
+  navset_card_tab(
+    id = "tabs",
+    
+    # First tab for Data Explorer
+    nav_panel(
+      "Data Explorer",
+      card(
+        h5(textOutput("map_title")), 
+        leafletOutput("map", height = 300),
+        plotlyOutput("time_series_plot", height = "150px") # Add time series plot below the map
+      )
+    ),
+    
+    # Second tab for Info
+    nav_panel(
+      "Info",
+      card(
+        h5("Information"),   # Title for the Info card
+        
+        p("The primary input data consists of daily data extracted from the database of the National
+Meteorological Administration of Romania."),
+        p("For 1901–1960, the daily Tavg was derived as the
+average of three climatological observations taken at 06, 12, and 18 UTC, then a Köppen
+coefficient was applied, calculated based on the Tmin and month of the year. <br> For 1961-2023,
+the daily Tavg was calculated as the arithmetic average of four climatological observations
+recorded at 00, 06, 12, and 18 UTC."),
+p("For the entire period of analysis, daily PREC was the total accumulation from 18 UTC on the
+previous day to 18 UTC on the current day, with the timestamp corresponding to the end of
+the 24-hour accumulation period."),
+p("The quality control, gap-filling, and homogenization were performed using the methodology implemented in Climatol.")
+        # Add more content as needed for the Info tab
+      )
+    )
   )
 )
