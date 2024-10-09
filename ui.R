1e71ffab7a2bc2024d7c2de5a4ccaeb4143e6ddd
alexdum <alexandru.dumitrescu@gmail.com>
2024-10-09 15:54:37 -0500

    tidy code

--- ui.R
+++ ui.R
@@ -1,9 +1,10 @@
 page_navbar(
   theme = bs_theme(version = 5),
 
-  # Add the canonical link inside the head tag
+  # Add the canonical link and noindex meta tag inside the head tag
   tags$head(
-    tags$link(rel = "canonical", href = "https://climate-insights.netlify.app/roclihom")
+    tags$link(rel = "canonical", href = "https://climate-insights.netlify.app/roclihom"),
+    
   ),
   
   
